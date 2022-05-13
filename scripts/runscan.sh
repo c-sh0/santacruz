e8396c43b5ebd3117759c63c22a8a74039c2d5e7
c-sh0 <csh.c0s0h0@gmail.com>
2022-05-13 20:49:28 +0000

    refactor

--- scripts/runscan.sh
+++ scripts/runscan.sh
@@ -4,24 +4,10 @@
 #
 # [/csh:]> date "+%D"
 # 05/11/22
-#---------------------------------------------------------------#
-# defaults
-_targetsf=''
-_rootdir=''
-_config=''
-_nmap2es=''
-_nmapparse=''
-_datadir=''
-_nmapdir=''
-_nucleidir=''
-_httpxnse=''
-_ntpldir=''
-_bin_nmap=''
-_bin_httpx=''
-_bin_nuclei=''
-_verbose=0
-
-# print help
+#
+#################
+# Help Function #
+#################
 show_help() {
 cat << _EOF_
 Usage: $0 [-d] [-t] -h -v
@@ -34,15 +20,18 @@ Options:
 -v, --verbose   :- Verbose output
 
 _EOF_
+
 exit 0
 }
 
-# date/time
+#################
+# Date Function #
+#################
 mkdate() {
 	_dtype=${1}
 
 	if [ "${_dtype}" == 'dt' ]; then
-		local _d=`date +"%Z: %Y-%m-%d %T.%3N"`
+		local _d=`date +"%Y-%m-%d %T.%3N (%Z)"`
 
 	elif [ "${_dtype}" == 'd' ]; then
 		local _d=`date +"%Y-%m-%d"`
@@ -51,73 +40,11 @@ mkdate() {
 	echo "${_d}"
 }
 
-# check config, set global vars
-check_setup() {
-	_config="${1}/conf/santacruz.yml"
-	_nmap2es="${1}/scripts/nmap2es.py"
-	_nmapparse="${1}/scripts/nmapparse.py"
-	_httpxnse="${1}/nmap_nse/httpx.nse"
-	_ntpldir="${1}/nuclei-templates"
-	_datadir="${1}/data"
-
-	# check directories
-	chk_dirs=(${_datadir} ${_ntpldir})
-	for _dir in "${chk_dirs[@]}"; do
-		if [ ! -d ${_dir} ]; then
-			echo "error: No such directory, (${_dir})"
-			exit -255
-		fi
-	done
-
-	# check files
-	chk_files=(${_config} ${_nmap2es} ${_nmapparse} ${_httpxnse})
-	for _file in "${chk_files[@]}"; do
-		if [ ! -f ${_file} ]; then
-			echo "error: No such file, (${_file})"
-			exit -255
-		fi
-	done
-
-	# create nmap output dir if not exist
-	_nmapdir="${_datadir}/nmap"
-	echo "[$(mkdate "dt")]: ${FUNCNAME[0]} [CMD]: mkdir -p ${_nmapdir}"
-	mkdir -p ${_nmapdir}
-	if [ ! -d ${_nmapdir} ]; then
-		echo "error: No such directory, (${_nmapdir})"
-		exit -255
-	fi
-
-	# create nuclei output dir if not exist
-	_nucleidir="${_datadir}/nuclei"
-	echo "[$(mkdate "dt")]: ${FUNCNAME[0]} [CMD]: mkdir -p ${_nucleidir}"
-	mkdir -p ${_nucleidir}
-	if [ ! -d ${_nucleidir} ]; then
-		echo "error: No such directory, (${_nucleidir})"
-		exit -255
-	fi
-
-	# tool vars
-	_tcount=`grep '^nmap-bin:\|^httpx-bin:\|^nuclei-bin:' ${_config} | awk 'END{print NR}'`
-	if [ "${_tcount}" -eq 3 ]; then
-		_bin_nmap=`grep '^nmap-bin:' ${_config} | awk '{print $2}'`
-		_bin_httpx=`grep '^httpx-bin:' ${_config} | awk '{print $2}'`
-		_bin_nuclei=`grep '^nuclei-bin:' ${_config} | awk '{print $2}'`
-
-		tools=(${_bin_nmap} ${_bin_httpx} ${_bin_nuclei})
-		for tool in "${tools[@]}"; do
-			if [ ! -f ${tool} ]; then
-				echo "error: No such file, (${_file}), check your configuration (${_config})"
-				exit -255
-			fi
-		done
-	else
-		echo "error: Missing required tools, check your configuration (${_config})"
-		exit -255
-	fi
-
-}
-
-# parse arguments
+######################
+# Parseargs Function #
+######################
+_rootdir=''
+_targetsf=''
 parse_args() {
 	_nargs=0
         args=${1}
@@ -130,11 +57,6 @@ parse_args() {
 
 			-t|--targets) shift
 				_targetsf=${1}
-				((_nargs++))
-			;;
-
-			-v|--verbose) shift
-				_verbose=1
 				((_nargs++))
 			;;
 
@@ -152,8 +74,7 @@ parse_args() {
 		esac
 	done
 
-	[ "${_verbose}" -eq 0 ] && ((_nargs++))
-	[ "${_nargs}" -ne 3 ] && show_help
+	[ "${_nargs}" -ne 2 ] && show_help
 
 	if [ ! -d ${_rootdir} ]; then
 		echo "error: No such directory, (${_rootdir})"
@@ -165,92 +86,160 @@ parse_args() {
 		exit -255
 	fi
 }
+parse_args "$@"
 
-# run discovery scan
-discovery_scan_01() {
-	# logfile
-	_logf="${1}"
-	_pscan_targets="${2}"
-
-	echo "[$(mkdate "dt")]: ${FUNCNAME[0]} START"
-
-	# nmap args
-	# (-V0 (silent), -n (No DNS resolution), -sn (No port scan)
-	_OPTS='-v0 -n -sn'
-	# TCP SYN Ping ports
-	_PS='-PS21-23,25,53,80,110-111,135,139,143,161,443,445,993,995,1723,3306,3389,5900,8080'
-	# UDP Ping ports
-	_PU='-PU53,67-69,111,123,135,137-139,161-162,445,500,514,520,631,1434,1900,4500,5353,49152'
-	# tuning options
-	_TN='--min-parallelism 100 --min-hostgroup 100 --min-rate 20000 --randomize-hosts --disable-arp-ping --max-retries 1'
-
-	# do scan
-	echo "[$(mkdate "dt")]: ${FUNCNAME[0]} [CMD]: ${_bin_nmap} ${_OPTS} ${_PS} ${_PU} ${_TN} -iL ${_targetsf} -oX ${_logf}"
-	${_bin_nmap} ${_OPTS} ${_PS} ${_PU} ${_TN} -iL ${_targetsf} -oX ${_logf}
-
-	# create portscan targets file
-	echo "[$(mkdate "dt")]: ${FUNCNAME[0]} [CMD]: ${_nmapparse} --file ${_logf} --data-type ip > ${_pscan_targets}"
-	${_nmapparse} --file ${_logf} --data-type ip > ${_pscan_targets}
-
-	echo "[$(mkdate "dt")]: ${FUNCNAME[0]} END"
-}
+###########
+# Globals #
+###########
+_scandate="$(mkdate "d")"
+_datadir="${_rootdir}/data"
+_confdir="${_rootdir}/conf"
+_scriptdir="${_rootdir}/scripts"
+_nsedir="${_rootdir}/nmap_nse"
+_nucleitpldir="${_datadir}/nuclei-templates"
+_nucleidir="${_datadir}/nuclei"
+_nmapdir="${_datadir}/nmap"
+_nmap2es="${_scriptdir}/nmap2es.py"
+_nmapparse="${_scriptdir}/nmapparse.py"
+_httpxnse="${_nsedir}/httpx.nse"
+#
+_dscan_xml="${_nmapdir}/dsicovery_scan-${_scandate}.xml"
+_pscan_xml="${_nmapdir}/portscan-${_scandate}.xml"
+_pscan_targets="${_nmapdir}/portscan-${_scandate}.targets"
+_nuclei_targets="${_nucleidir}/nuclei-${_scandate}.targets"
+#
+_nmap='/usr/local/bin/nmap'
+_httpx='/usr/local/bin/httpx'
+_nuclei='/usr/local/bin/nuclei'
 
 
-# run port scan
-port_scan_02() {
-	echo "[$(mkdate "dt")]: ${FUNCNAME[0]} START"
-	echo "[$(mkdate "dt")]: ${FUNCNAME[0]} END"
-}
-
-# run nuclei scan
-nuclei_scan_03() {
-	echo "[$(mkdate "dt")]: ${FUNCNAME[0]} START"
-	echo "[$(mkdate "dt")]: ${FUNCNAME[0]} END"
-}
-
-
-# Run scan
-run_scan() {
-	# date (log file name)
-	_logfd="$(mkdate "d")"
-	_discovery_log="${_nmapdir}/dsicovery_scan-${_logfd}.xml"
-	_portscan_log="${_nmapdir}/portscan-${_logfd}.xml"
-	_portscan_targets="${_nmapdir}/portscan-${_logfd}.hosts"
-
-	echo "[$(mkdate "dt")]: ${FUNCNAME[0]} START"
-
-	# check files
-	chk_files=(${_discovery_log} ${_portscan_log} ${_portscan_targets})
-	for _file in "${chk_files[@]}"; do
-		if [ -f ${_file} ]; then
-			echo "[$(mkdate "dt")]: ${FUNCNAME[0]} Found ${_file}, Scan already running?, exit()"
+#####################
+# Pre-scan function #
+#####################
+pre_scan() {
+	chk_dirs=(${_datadir} ${_confdir} ${_scriptdir} ${_nsedir} ${_nucleitpldir})
+	for _dir in "${chk_dirs[@]}"; do
+		if [ ! -d ${_dir} ]; then
+			echo "[$(mkdate "dt")] ${FUNCNAME[0]}() Error: No such directory, (${_dir})"
 			exit -255
 		fi
 	done
 
-	# Stage 1
-	discovery_scan_01 "${_discovery_log}" "${_portscan_targets}"
+	# check files
+	chk_files=(${_nmap2es} ${_nmapparse} ${_httpxnse})
+	for _file in "${chk_files[@]}"; do
+		if [ ! -f ${_file} ]; then
+			echo "[$(mkdate "dt")] ${FUNCNAME[0]}() Error: No such file, (${_file})"
+			exit -255
+		fi
+	done
 
-	# Stage 2
-	port_scan_02 "${_portscan_log}" "${_portscan_targets}"
+	# tools
+	tools=(${_nmap} ${_httpx} ${_nuclei})
+	for tool in "${tools[@]}"; do
+		if [ ! -f ${tool} ]; then
+			echo "[$(mkdate "dt")] ${FUNCNAME[0]}() Error: No such file, (${_file})"
+			exit -255
+		fi
+	done
 
-	# Stage 3
-	nuclei_scan_03 "${_logfd}"
+	# create nmap output dir if not exist
+	if [ ! -d ${_nmapdir} ]; then
+		echo "[$(mkdate "dt")]: ${FUNCNAME[0]} [CMD]: mkdir -p ${_nmapdir}"
+		mkdir -p -m 700 ${_nmapdir}
+		if [ ! -d ${_nmapdir} ]; then
+			echo "[$(mkdate "dt")] ${FUNCNAME[0]}() Error: No such directory, (${_nmapdir})"
+			exit -255
+		fi
+	fi
 
-	echo "[$(mkdate "dt")]: ${FUNCNAME[0]} END"
+	# create nuclei output dir if not exist
+	if [ ! -d ${_nucleidir} ]; then
+		echo "[$(mkdate "dt")]: ${FUNCNAME[0]} [CMD]: mkdir -p ${_nucleidir}"
+		mkdir -p -m 700 ${_nucleidir}
+		if [ ! -d ${_nucleidir} ]; then
+			echo "[$(mkdate "dt")] ${FUNCNAME[0]}() Error: No such directory, (${_nucleidir})"
+			exit -255
+		fi
+	fi
+
+	# check for exsiting output files
+	chk_files=(${_dscan_xml} ${_pscan_xml} ${_pscan_targets} ${_nuclei_targets})
+	for _file in "${chk_files[@]}"; do
+		if [ -f ${_file} ]; then
+			echo "[$(mkdate "dt")] ${FUNCNAME[0]} Found ${_file}, Scan already running?, exit()"
+			exit -255
+		fi
+	done
+
 }
 
-#---------------------------------------------------------------#
-# main
-#---------------------------------------------------------------#
-parse_args "$@"
-check_setup "${_rootdir}"
+#################
+# Scan function #
+#################
+run_scan() {
+	###############
+	# Nmap notes:
+	# * For some reason the --open flag needs one of the last optionsotherwise,
+	#   nmap will still write filtered port information into the log files
+	#   this will result in HUGE log files when scanning a lot of hosts
+	# * Don't use -v0 (silent) flag, instead redirect stdio to /dev/null otherwise
+	#   nmap will still write filtered port information to the output log files
+	#   regardless if --open has been set. https://github.com/nmap/nmap/issues/236
+	###############
+	echo "[$(mkdate "dt")] ${FUNCNAME[0]}() START"
 
-# lock file
+	#------------------------------------------------------------------------------------#
+	# Nmap discovery scan
+	#------------------------------------------------------------------------------------#
+	echo "[$(mkdate "dt")] ${FUNCNAME[0]}() [CMD]: Nmap Discovery scan, Starting..."
+	echo "[$(mkdate "dt")] ${FUNCNAME[0]}() [CMD]: Logfile (${_dscan_xml})"
+
+		${_nmap} \
+			-n -sn \
+			-PS21-23,25,53,80,110-111,135,139,143,161,443,445,993,995,1723,3306,3389,5900,8080 \
+			-PU53,67-69,111,123,135,137-139,161-162,445,500,514,520,631,1434,1900,4500,5353,49152 \
+			--min-parallelism 100 --min-hostgroup 100 --min-rate 20000 --randomize-hosts --disable-arp-ping --max-retries 1 \
+		-iL ${_targetsf} -oX ${_dscan_xml} > /dev/null
+
+	echo "[$(mkdate "dt")] ${FUNCNAME[0]}() [CMD]: Nmap discovery scan, Complete."
+
+	# Create portscan targets file
+	echo "[$(mkdate "dt")] ${FUNCNAME[0]}() [CMD]: Create Portscan targets (${_pscan_targets})"
+	${_nmapparse} --file ${_dscan_xml} --data-type ip > ${_pscan_targets}
+
+	#------------------------------------------------------------------------------------#
+	# Nmap port scan (full port scan)
+	#------------------------------------------------------------------------------------#
+	echo "[$(mkdate "dt")] ${FUNCNAME[0]}() [CMD]: Nmap Portscan, Starting..."
+	echo "[$(mkdate "dt")] ${FUNCNAME[0]}() [CMD]: Logfile (${_pscan_xml})"
+
+		${_nmap} \
+			 -sS -p- \
+			--script=${_nsedir}/ --script-timeout=3 \
+			--min-parallelism 100 --min-rate 20000 --min-hostgroup 100 --randomize-hosts \
+		--open -iL ${_pscan_targets} -oX ${_pscan_xml} > /dev/null
+
+	echo "[$(mkdate "dt")] ${FUNCNAME[0]}() [CMD]: Nmap portscan, Complete."
+
+	# Create nuclei targets file (nmap httpx.nse results)
+	echo "[$(mkdate "dt")] ${FUNCNAME[0]}() [CMD]: Create Nuclei targets (${_nuclei_targets})"
+	${_nmapparse} --file ${_pscan_xml} --data-type httpx > ${_nuclei_targets}
+
+	#------------------------------------------------------------------------------------#
+	# Nuclei scan
+	#------------------------------------------------------------------------------------#
+
+	echo "[$(mkdate "dt")] ${FUNCNAME[0]}() END"
+}
+
+#############
+# lock file #
+#############
 _lockf="${_datadir}/.scan.lock"
 if [ -e "${_lockf}" ]; then
 	if [[ $(kill -0 `cat ${_lockf}`) -eq 0 ]]; then
-		echo "[$(mkdate "dt")]: Found ${_lockf}, exit()"
+		echo "[$(mkdate "dt")] Found ${_lockf}, exit()"
 		exit -1
 	fi
 fi
@@ -258,8 +247,11 @@ fi
 trap "rm -f ${_lockf}; exit" INT TERM EXIT
 echo $$ > ${_lockf}
 
+##############
+# Start Scan #
+##############
+pre_scan
 run_scan
 
-
-exit 1
+exit 0
 
