b50bd6c5caebc1411dd968f3e497a51d77a55a17
c-sh0 <csh.c0s0h0@gmail.com>
2022-05-26 12:28:02 +0000

    Logging

--- scripts/runscan.sh
+++ scripts/runscan.sh
@@ -126,13 +126,13 @@ _nuclei_tpldir="${_datadir}/nuclei-templates"
 # Pre-scan function #
 #####################
 pre_scan() {
-	wr_mesg "${FUNCNAME[0]}():${LINENO}, [INFO] - start"
+	wr_mesg "${FUNCNAME[0]}():${LINENO}, [INFO] ${FUNCNAME[0]}(), start"
 
 	# check directories
 	chk_dirs=(${_datadir} ${_confdir} ${_scriptdir} ${_nmap_rundir} ${_nuclei_tpldir})
 	for _dir in "${chk_dirs[@]}"; do
 		if [ ! -d ${_dir} ]; then
-			wr_mesg "${FUNCNAME[0]}():${LINENO}, [ERROR] - directory does not exist, ${_dir}"
+			wr_mesg "${FUNCNAME[0]}():${LINENO}, [ERROR] ${FUNCNAME[0]}(), directory does not exist: ${_dir}"
 			exit -255
 		fi
 	done
@@ -141,7 +141,7 @@ pre_scan() {
 	chk_files=(${_nmapparse} ${_httpxnse} ${_configf} ${_nucleiconf})
 	for _file in "${chk_files[@]}"; do
 		if [ ! -f ${_file} ]; then
-			wr_mesg "${FUNCNAME[0]}():${LINENO}, [ERROR] - file does not exist, ${_file}"
+			wr_mesg "${FUNCNAME[0]}():${LINENO}, [ERROR] ${FUNCNAME[0]}(), file does not exist: ${_file}"
 			exit -255
 		fi
 	done
@@ -150,7 +150,7 @@ pre_scan() {
 	tools=(${_nmap_bin} ${_httpx_bin} ${_nuclei})
 	for tool in "${tools[@]}"; do
 		if [ ! -f ${tool} ]; then
-			wr_mesg "${FUNCNAME[0]}():${LINENO}, [ERROR] - file does not exist, ${_file}"
+			wr_mesg "${FUNCNAME[0]}():${LINENO}, [ERROR] ${FUNCNAME[0]}(), file does not exist: ${_file}"
 			exit -255
 		fi
 	done
@@ -159,23 +159,23 @@ pre_scan() {
 	log_dirs=(${_logdir} ${_nmap_logdir} ${_httpx_logdir} ${_nuclei_logdir})
 	for _dir in "${log_dirs[@]}"; do
 		if [ ! -d ${_dir} ]; then
-			wr_mesg "${FUNCNAME[0]}():${LINENO}, [CMD] - creating directory, ${_dir}"
+			wr_mesg "${FUNCNAME[0]}():${LINENO}, [CMD] ${FUNCNAME[0]}(), creating directory: ${_dir}"
 			mkdir -p -m 700 ${_dir}
 			if [ ! -d ${_dir} ]; then
-				wr_mesg "${FUNCNAME[0]}():${LINENO}, [ERROR] - unable to create directory, ${_dir}"
+				wr_mesg "${FUNCNAME[0]}():${LINENO}, [ERROR] ${FUNCNAME[0]}(), unable to create directory: ${_dir}"
 				exit -255
 			fi
 		fi
 	done
 
-	wr_mesg "${FUNCNAME[0]}():${LINENO}, [INFO] - end"
+	wr_mesg "${FUNCNAME[0]}():${LINENO}, [INFO] ${FUNCNAME[0]}(), end"
 }
 
 #################
 # Scan function #
 #################
 run_scan() {
-	wr_mesg "${FUNCNAME[0]}():${LINENO}, [INFO] - start"
+	wr_mesg "${FUNCNAME[0]}():${LINENO}, [INFO] ${FUNCNAME[0]}(), start"
 
 	#------------------------------------------------------------------------------------#
 	# targets/log file vars
@@ -196,18 +196,18 @@ run_scan() {
 	chk_files=(${_dscan_log} ${_pscan_log} ${_nmap_targets} ${_httpx_log} ${_httpx_targets})
 	for _file in "${chk_files[@]}"; do
 		if [ -f ${_file} ]; then
-			wr_mesg "[WARN] ${FUNCNAME[0]}():${LINENO} - ${_file} found, Scan already running?, exit()"
+			wr_mesg "${FUNCNAME[0]}():${LINENO}, [WARN] ${FUNCNAME[0]}, found: ${_file}, Scan already running?, exit()"
 			exit -255
 		fi
 	done
 
 	# create nmap excludes file if not exist
 	if [ ! -f ${_nmap_excludef} ]; then
-		wr_mesg "${FUNCNAME[0]}():${LINENO}, [CMD] - creating, ${_nmap_excludef}"
+		wr_mesg "${FUNCNAME[0]}():${LINENO}, [CMD] ${FUNCNAME[0]}, create: ${_nmap_excludef}"
 		touch ${_nmap_excludef}
 
 		if [ ! -f ${_nmap_excludef} ]; then
-			wr_mesg "${FUNCNAME[0]}():${LINENO}, [ERROR] - file does not exist, ${_nmap_excludef}"
+			wr_mesg "${FUNCNAME[0]}():${LINENO}, [ERROR] ${FUNCNAME[0]}, file does not exist: ${_nmap_excludef}"
 			exit -255
 		fi
 	fi
@@ -232,21 +232,21 @@ run_scan() {
 	#------------------------------------------------------------------------------------#
 	# Nmap discovery scan (Phase 00)
 	#------------------------------------------------------------------------------------#
-	wr_mesg "${FUNCNAME[0]}():${LINENO}, [NMAP] - discovery_scan, started ..."
+	wr_mesg "${FUNCNAME[0]}():${LINENO}, [NMAP] discovery_scan, started ..."
 
 		${_nmap_bin} \
-			-v0 -n -sn \
+			-v0 -sn \
 			-PS21-23,25,53,80,110-111,135,139,143,161,443,445,587,993,995,1025,1723,3306,3389,5666,5900,8080,8443,9090 \
 			-PU53,67-69,111,123,135,137-139,161-162,445,500,514,520,631,1434,1900,4500,5353,49152 \
 			--min-parallelism 32 --min-rate 10000 --randomize-hosts --disable-arp-ping --max-retries 4 \
 		-iL ${_targetsf} --excludefile ${_nmap_excludef} -oX ${_dscan_log} > /dev/null
 
-	 wr_mesg "${FUNCNAME[0]}():${LINENO}, [NMAP] - discovery_scan, completed (log: ${_dscan_log})"
+	 wr_mesg "${FUNCNAME[0]}():${LINENO}, [NMAP] discovery_scan, completed, log: ${_dscan_log}"
 
 	#------------------------------------------------------------------------------------#
 	# Create port scan targets file
 	#------------------------------------------------------------------------------------#
-	wr_mesg "${FUNCNAME[0]}():${LINENO}, [NMAP] - port_scan, creating targets file: ${_nmap_targets}"
+	wr_mesg "${FUNCNAME[0]}():${LINENO}, [NMAP] port_scan, creating targets file: ${_nmap_targets}"
 	${_nmapparse} --file ${_dscan_log} --output ip > ${_nmap_targets}
 
 
@@ -256,26 +256,26 @@ run_scan() {
 	# Use custom nmap-services file for default port scanning. (--datadir)
 	# https://nmap.org/book/nmap-services.html
 	#------------------------------------------------------------------------------------#
-	wr_mesg "${FUNCNAME[0]}():${LINENO}, [NMAP] - port_scan, started ..."
+	wr_mesg "${FUNCNAME[0]}():${LINENO}, [NMAP] port_scan, started ..."
 
 		${_nmap_bin} -v0 -sS --open --datadir ${_nmap_rundir} \
-			--min-parallelism 64 --min-hostgroup 256 --min-rate 10000 --defeat-rst-ratelimit --host-timeout 10m --randomize-hosts \
+			--min-parallelism 64 --min-hostgroup 128 --min-rate 10000 --defeat-rst-ratelimit --host-timeout 10m --randomize-hosts \
 		-iL ${_nmap_targets} --excludefile ${_nmap_excludef} -oX ${_pscan_log} > /dev/null
 
-	 wr_mesg "${FUNCNAME[0]}():${LINENO}, [NMAP] - port_scan, completed (log: ${_pscan_log})"
+	 wr_mesg "${FUNCNAME[0]}():${LINENO}, [NMAP] port_scan, completed, log: ${_pscan_log}"
 
 	#------------------------------------------------------------------------------------#
 	# Ferret out hosts with an IDS and exclude them from future scans. If the report
 	# shows hosts with 100 or more ports as open, This is a pretty good indication that
 	# there is a security mechanism in place for that host.
 	#------------------------------------------------------------------------------------#
-	wr_mesg "${FUNCNAME[0]}():${LINENO}, [NMAP] - excluding hosts with open ports >= 100"
+	wr_mesg "${FUNCNAME[0]}():${LINENO}, [INFO] searching for hosts with open ports >= 100 ..."
 	${_nmapparse} --file ${_pscan_log} --output pcount -gt 50 | cut -d, -f2 | sort -t. -k3,3n -k4,4n > ${_nmap_exclude_tmp}
 
 	for _ip_addr in `cat ${_nmap_exclude_tmp}`; do
 		_found=`grep -c -w ${_ip_addr} ${_nmap_excludef}`
 		if [ ${_found} -eq 0 ]; then
-			wr_mesg "${FUNCNAME[0]}():${LINENO}, [INFO] - adding ${_ip_addr} to ${_nmap_excludef}"
+			wr_mesg "${FUNCNAME[0]}():${LINENO}, [INFO] adding ${_ip_addr} to ${_nmap_excludef}"
 			echo "${_ip_addr}" >> ${_nmap_excludef}
 		fi
 	done
@@ -289,7 +289,7 @@ run_scan() {
 	# --skip-gt (Skip hosts who report >= 100 open ports, chances are that there is a
 	# security mechanism in place for that host)
 	#------------------------------------------------------------------------------------#
-	wr_mesg "${FUNCNAME[0]}():${LINENO}, [HTTPX] - httpx_scan, creating targets file: ${_httpx_targets}"
+	wr_mesg "${FUNCNAME[0]}():${LINENO}, [HTTPX] httpx_scan, creating targets, file: ${_httpx_targets}"
 	${_nmapparse} --file ${_pscan_log} --output httpx --skip-gt 100 > ${_httpx_targets}
 
 	#------------------------------------------------------------------------------------#
@@ -297,16 +297,16 @@ run_scan() {
 	# see this discussion:
 	# https://github.com/projectdiscovery/httpx/discussions/633
 	#------------------------------------------------------------------------------------#
-	wr_mesg "${FUNCNAME[0]}():${LINENO}, [HTTPX] - httpx_scan, started ..."
+	wr_mesg "${FUNCNAME[0]}():${LINENO}, [HTTPX] httpx_scan, started ..."
 	export GODEBUG=tls10default=1
 	${_httpx_bin} -silent -nc -sc -td -tls-grab -title -fhr -ec -asn -location -web-server -jarm -maxr 5 -timeout 3 -list ${_httpx_targets} -json > ${_httpx_log}
 
-	wr_mesg "${FUNCNAME[0]}():${LINENO}, [HTTPX] - httpx_scan, completed (log: ${_httpx_log})"
+	wr_mesg "${FUNCNAME[0]}():${LINENO}, [HTTPX] httpx_scan, completed, log: ${_httpx_log}"
 
 	#------------------------------------------------------------------------------------#
 	# Create nuclei targets from httpx results
 	#------------------------------------------------------------------------------------#
-	wr_mesg "${FUNCNAME[0]}():${LINENO}, [NUCLEI] - nuclei_scan, creating targets file: ${_nuclei_targets}"
+	wr_mesg "${FUNCNAME[0]}():${LINENO}, [NUCLEI] nuclei_scan, creating targets, file: ${_nuclei_targets}"
 	cat ${_httpx_log} | jq '.url' | sed -e 's/"//g' > ${_nuclei_targets}
 
 
@@ -315,7 +315,7 @@ run_scan() {
 	#------------------------------------------------------------------------------------#
 	#------------------------------------------------------------------------------------#
 
-	wr_mesg "${FUNCNAME[0]}():${LINENO}, [INFO] - end"
+	wr_mesg "${FUNCNAME[0]}():${LINENO}, [INFO] ${FUNCNAME[0]}(), end"
 }
 
 #############
